4ffdfca4020bd7b7a951077646de257571b0da67
tomasnovoa <dioshydrox@gmail.com>
2023-06-07 13:08:38 -0400

    avanzanding

--- Juego de Vaqueros/Assets/Scripts/Bullet.cs
+++ Juego de Vaqueros/Assets/Scripts/Bullet.cs
@@ -5,23 +5,55 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 10f;  // Velocidad del proyectil
-    public bool moveRight = true;  // Variable para controlar la dirección del movimiento
-
+    private Transform playerTransform;  // Referencia al transform del jugador
+    public float DestroyDelay = 3;
     private Rigidbody2D rb;
 
     private void Start()
     {
+        DestroyProjectile(3f);
         rb = GetComponent<Rigidbody2D>();
+
+        // Buscar el transform del jugador por su nombre
+        GameObject playerObject = GameObject.Find("Player");
+
+        if (playerObject != null)
+        {
+            playerTransform = playerObject.transform;
+        }
+        else
+        {
+            Debug.LogError("Player");
+        }
+
+
+        // Verificar si se encontró el transform del jugador
+        if (playerTransform != null)
+        {
+            // Calcular la dirección del movimiento en base a la escala del jugador
+            Vector2 movement = Vector2.right;  // Dirección predeterminada
+
+            if (playerTransform.localScale.x < 0)
+            {
+                // Jugador mira hacia la izquierda, invertir la dirección del movimiento
+                movement = Vector2.left;
+            }
+
+            // Mover el proyectil en la dirección calculada
+            rb.velocity = movement * speed;
+        }
     }
 
     private void Update()
     {
-        // Calcular la dirección del movimiento en base a la variable moveRight
-        Vector2 movement = moveRight ? Vector2.right : Vector2.left;
-
-        // Mover el proyectil en la dirección calculada
-        rb.velocity = movement * speed;
-
+       
     }
+
+    private void DestroyProjectile(float DestroyDelay)
+    {
+        // Destruir el proyectil después de un cierto tiempo (delay)
+        Destroy(gameObject, DestroyDelay);
+    }
+
 
 }
